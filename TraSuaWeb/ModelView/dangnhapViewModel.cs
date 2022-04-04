4ac74ae3e33875a6dc92f17e00bb834d15579225
Nguyen Hien <nguyenhien111110@gmail.com>
2022-04-04 18:43:47 +0700

    first commit

---
+++ TraSuaWeb/ModelView/dangnhapViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TraSuaWeb.ModelView
+{
+    public class dangnhapViewModel
+    {
+        [Key]
+        [MaxLength(50)]
+        [Required(ErrorMessage = "Vui lòng nhập Email")]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        [Display(Name = "Dia chi Email")]
+        public string Email { get; set; }
+
+        [Display(Name = "Mặt khẩu")]
+        [Required(ErrorMessage = "Vui lòng nhập mật khẩu")]
+        [MinLength(8, ErrorMessage = "Mật khẩu chỉ được sử dụng 8 ký tự")]
+        public string Password { get; set; }
+    }
+}
